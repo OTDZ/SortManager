1386c64be2baec9be8d4d9124e340c728a5de9e5
OTDZ <omartehami6@gmail.com>
2022-06-29 19:05:23 +0100

    Randomly generated array is sorted and relevant info is displayed to user

--- Sorter/src/main/java/SorterApplication.java
+++ Sorter/src/main/java/SorterApplication.java
@@ -8,26 +8,42 @@ public class SorterApplication {
 
         // Sort algorithms implemented so far:
         // 1. Bubble Sort
-
+        BubbleSorter bubbleSorter = new BubbleSorter();
 
         // Getting user input
         Scanner scanner = new Scanner(System.in);
         System.out.println("Choose a sorting algorithm: ");
         System.out.println("1. Bubble Sort");
-        System.out.println();
         int algorithm = scanner.nextInt();
 
         System.out.print("Enter array length: ");
         int arrayLength = scanner.nextInt();
 
+        // Initialize arrays
+        int[] arrayToSort = new int[arrayLength];
+        int[] sortedArray = new int [arrayLength];
+
         // Randomly generate array
-        int[] array = new int[arrayLength];
         Random random = new Random() ;
         for (int i = 0; i < arrayLength; i++){
             // Range is -200 to 200
-            array[i] = random.nextInt(-200, 200);
+            arrayToSort[i] = random.nextInt(-200, 200);
         }
 
+        // Output info to user
+        System.out.println();
+        System.out.println("The unsorted array: " + Arrays.toString(arrayToSort));
+        System.out.print("The selected sorting algorithm is ");
+
+        switch (algorithm){
+            case 1:
+                System.out.println("Bubble Sort");
+                sortedArray = bubbleSorter.sortArray(arrayToSort);
+        }
+
+        System.out.println();
+        System.out.println("The sorted array: " + Arrays.toString(sortedArray));
+        
 
 
     }
