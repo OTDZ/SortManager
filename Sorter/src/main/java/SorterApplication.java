31c4ef57f477a3c0d9ae4bff64af3b6da0f9ab62
OTDZ <omartehami6@gmail.com>
2022-06-29 18:52:12 +0100

    Created SorterApplication class

--- Sorter/src/main/java/SorterApplication.java
+++ Sorter/src/main/java/SorterApplication.java
@@ -1,7 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class SorterApplication {
 
     public static void main(String[] args) {
-        
+
+        // Sort algorithms implemented so far:
+        // 1. Bubble Sort
+
+
     }
 
 }
