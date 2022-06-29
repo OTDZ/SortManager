6f3c51195193fc7fd4110422c49df1d0fd881621
OTDZ <omartehami6@gmail.com>
2022-06-29 18:35:26 +0100

    Implemented bubble sort in the sortArray method in BubbleSorter

--- Sorter/src/main/java/BubbleSorter.java
+++ Sorter/src/main/java/BubbleSorter.java
@@ -1,2 +1,19 @@
-public class BubbleSorter {
+public class BubbleSorter implements Sorter{
+
+    @Override
+    public int[] sortArray(int[] arrayToSort) {
+
+        for (int i = 0; i < arrayToSort.length-1; i++){
+            for (int j = 0; j < arrayToSort.length-i-1; j++){
+                if (arrayToSort[j] > arrayToSort[j + 1]) {
+                    int temp = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j + 1];
+                    arrayToSort[j + 1] = temp;
+                }
+            }
+        }
+
+        return arrayToSort;
+    }
+
 }
