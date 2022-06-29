fe7f5df46c48a12943a0f1bee02baa45fa24198d
OTDZ <omartehami6@gmail.com>
2022-06-29 18:31:36 +0100

    Created BubbleSorter class and BubbleSorterTest class

--- Sorter/src/main/java/Sorter.java
+++ Sorter/src/main/java/Sorter.java
@@ -1,5 +1,5 @@
 public interface Sorter {
 
     int[] sortArray(int[] arrayToSort);
-    
+
 }
---
+++ Sorter/src/test/java/BubbleSorterTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSorterTest {
+
+    BubbleSorter bubbleSorter;
+
+    @BeforeEach
+    void setUp() {
+        bubbleSorter = new BubbleSorter();
+    }
+
+    
+}
