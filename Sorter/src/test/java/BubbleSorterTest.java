d79cc38d85c73c3a3a1fa21ba0e0e64c930580c7
OTDZ <omartehami6@gmail.com>
2022-06-29 18:32:53 +0100

    Created simple bubble sort test case

--- Sorter/src/test/java/BubbleSorterTest.java
+++ Sorter/src/test/java/BubbleSorterTest.java
@@ -1,4 +1,6 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +13,12 @@ class BubbleSorterTest {
         bubbleSorter = new BubbleSorter();
     }
 
-    
+    @Test
+    void testSimpleBubbleSort(){
+        int[] sort = {3,1,2,4};
+        int[] expected = {1,2,3,4};
+        Assertions.assertArrayEquals(expected, bubbleSorter.sortArray(sort));
+    }
+
+
 }
