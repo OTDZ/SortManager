46031921c6a2d2f359b27288cf7d7085ee5ab16c
OTDZ <omartehami6@gmail.com>
2022-06-30 13:36:05 +0100

    Implemented merge sort algorithm

--- Sorter/src/main/java/MergeSorter.java
+++ Sorter/src/main/java/MergeSorter.java
@@ -2,7 +2,66 @@ public class MergeSorter implements Sorter{
 
     @Override
     public int[] sortArray(int[] arrayToSort) {
-        
+        if (arrayToSort.length > 1){
+
+            // Midpoint
+            int mid = arrayToSort.length/2;
+
+            // Left half
+            int[] left = new int[mid];
+            for (int i = 0; i < mid; i++){
+                left[i] = arrayToSort[i];
+            }
+
+            // Right half
+            int[] right = new int[arrayToSort.length - mid];
+            for (int i = 0; i < mid; i++){
+                right[i] = arrayToSort[i+mid];
+            }
+
+            sortArray(left);
+            sortArray(right);
+
+            // Index counter for left
+            int i = 0;
+            // Index counter for right
+            int j = 0;
+            // Index counter for initial array
+            int k = 0;
+
+            while (i < left.length && j < right.length){
+
+                if (left[i] < right[j]){
+                    arrayToSort[k] = left[i];
+                    i++;
+                }
+                else{
+                    arrayToSort[k] = right[j];
+                    j++;
+                }
+                k++;
+
+            }
+
+            while (i < left.length){
+
+                arrayToSort[k] = left[i];
+                i++;
+                k++;
+
+            }
+
+            while (j < right.length){
+
+                arrayToSort[k] = right[j];
+                j++;
+                k++;
+
+            }
+
+        }
+
+        return arrayToSort;
 
     }
 
