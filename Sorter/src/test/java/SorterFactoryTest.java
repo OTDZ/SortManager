3df5d7fdaa9ddaa71f59549b475643f1b0389e13
OTDZ <omartehami6@gmail.com>
2022-07-04 16:36:28 +0100

    Wrote simple test case for buildSorter function

---
+++ Sorter/src/test/java/SorterFactoryTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SorterFactoryTest {
+
+    SorterFactory sorterFactory;
+
+    @BeforeEach
+    void setUp() {
+        sorterFactory = new SorterFactory();
+    }
+
+    @Test
+    void testSorterFactoryBubble(){
+        BubbleSorter bubbleSorter = new BubbleSorter();
+        Assertions.assertEquals(bubbleSorter, sorterFactory.buildSorter(Sorter.BUBBLE));
+    }
+
+}
