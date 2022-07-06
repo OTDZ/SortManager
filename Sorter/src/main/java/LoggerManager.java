f628aea2f57e1b1585b1d44584055a7526d405c5
OTDZ <omartehami6@gmail.com>
2022-07-06 14:41:17 +0100

    Created a Logger

---
+++ Sorter/src/main/java/LoggerManager.java
@@ -0,0 +1,8 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerManager {
+
+    public static Logger logger = LogManager.getLogger("SortManager Logger");
+
+}
