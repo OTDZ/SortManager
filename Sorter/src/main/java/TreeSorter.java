e8f80247890c3905da83773f201127d7710de79b
OTDZ <omartehami6@gmail.com>
2022-07-07 15:09:31 +0100

    Created TreeSorter class

---
+++ Sorter/src/main/java/TreeSorter.java
@@ -0,0 +1,13 @@
+public class TreeSorter implements Sorter{
+
+    @Override
+    public int[] sortArray(int[] arrayToSort) {
+        return new int[0];
+    }
+
+    @Override
+    public String toString(){
+        return "tree sort";
+    }
+
+}
