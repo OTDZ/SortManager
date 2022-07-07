47e32a7272ffb5941ef19c4fac427a080ceb4bfe
OTDZ <omartehami6@gmail.com>
2022-07-07 15:17:42 +0100

    Created BinarySearchTree class

--- Sorter/src/main/java/TreeSorter.java
+++ Sorter/src/main/java/TreeSorter.java
@@ -2,7 +2,10 @@ public class TreeSorter implements Sorter{
 
     @Override
     public int[] sortArray(int[] arrayToSort) {
-        return new int[0];
+
+        // Create a binary search tree
+
+
     }
 
     @Override
