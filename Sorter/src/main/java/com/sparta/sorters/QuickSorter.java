a1c11553dfafa3b4b88e9fa65ce6b2c07c8ca1bf
OTDZ <omartehami6@gmail.com>
2022-07-08 14:42:11 +0100

    Created a QuickSorter class

---
+++ Sorter/src/main/java/com/sparta/sorters/QuickSorter.java
@@ -0,0 +1,10 @@
+package com.sparta.sorters;
+
+public class QuickSorter implements Sorter{
+
+    @Override
+    public int[] sortArray(int[] arrayToSort) {
+        return new int[0];
+    }
+    
+}
